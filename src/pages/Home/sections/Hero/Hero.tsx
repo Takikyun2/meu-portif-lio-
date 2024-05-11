b6b1ad3c7a3f4a0e36475a0c6b2d88b96665aace
Takikyun2 <darksd016@gmail.com>
2024-05-10 23:43:32 -0300

    fix: Hero components UI

--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -1,21 +1,31 @@
 import { styled } from "@mui/system";
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Typography, Box } from "@mui/material";
 import Profile from "../../../../assets/images/ryoyamada.jpg";
 import DownloadIcon from "@mui/icons-material/Download";
 import EmailIcon from "@mui/icons-material/Email";
 import StyledButton from "../../../../components/StyledButton/StyledButton.js";
 
+
 const Hero = () => {
-  const StyledHero = styled("div")(() => ({
+
+  const StyledHero = styled("div")(({ theme }) => ({
     backgroundColor: "#292929",
     height: "100vh",
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center", 
+    alignItems: "center",
+    [theme.breakpoints.up('xs')]: {
+      // <= mobile
+      paddingTop: "100px"
+    },
+
+    [theme.breakpoints.up('xs')]: {
+      // >= mobile
+      paddingTop: "0px"
+    },
   }));
 
   const StyledImg = styled("img")(() => ({
-    width: "80%",
+    width: "75%",
     borderRadius: "10%",
   }));
 
@@ -25,16 +35,36 @@ const Hero = () => {
         <Container maxWidth="lg">
           <Grid container spacing={2}>
             <Grid item xs={12} md={5}>
-              <StyledImg src={Profile} />
+              {/*<Box position="relative">
+                <Box position="relative" witdh="150%" top="-100" right="0">
+                  <AnimatedBackground />
+                </Box>
+              </Box>*/}
+              <Box position="relative">
+                <Box position="relative" textAlign="center">
+                  <StyledImg src={Profile} />
+                </Box>
+              </Box>
             </Grid>
             <Grid item xs={12} md={7}>
               <Typography color="primary" variant="h1" textAlign="center">
                 Hello i'm CLayton
               </Typography>
-              <Typography color="secondary" variant="h2" textAlign="center" pb={2}>
+              <Typography
+                color="secondary"
+                variant="h2"
+                textAlign="center"
+                pb={2}
+              >
                 i'm a apprentice developer
               </Typography>
-              <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
+              <Grid
+                container
+                display="flex"
+                justifyContent="center"
+                spacing={3}
+                pt={3}
+              >
                 <Grid
                   item
                   xs={12}
